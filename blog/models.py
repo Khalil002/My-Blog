07cbca202d7194ba194de9213147618669f332f7
Khalil El Hage Kassem <90007227+Khalil002@users.noreply.github.com>
2026-06-06 22:48:37 +0200

    Added posts and comments
    Created post and comments
    - models
    - views
    - templates
    - urls

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,21 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+    latest_post_list = Post.objects.order_by("-pub_date")[:5]
+    context = {"latest_post_list": latest_post_list}
+    return render(request, "blog/index.html", context)
+
+def detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, "blog/detail.html", {"post": post})
+
+
+
+
+
+
+
+# Leave the rest of the views (detail, results, vote) unchanged
